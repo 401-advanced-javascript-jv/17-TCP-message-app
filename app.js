ec39e0964bfcde724b5ef8dcc2fcd7c193a8530a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-04 20:55:40 -0700

    refactor app to use promises and modularized functions, too

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const readFile = (file) => {
+  return fs.readFile(file).catch((error) => {
+    throw error;
+  });
+};
+
+const modifyContents = (data) => {
+  let text = data.toString();
+  let upperText = text.toUpperCase();
+  let buffer = Buffer.from(upperText);
+  return buffer;
+};
+
+const writeFile = (filename, buffer) => {
+  return fs.writeFile(filename, buffer).catch((error) => {
+    throw error;
+  });
+};
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
+  readFile(file)
+    .then((data) => {
+      let outBuffer = modifyContents(data);
+      return writeFile();
+    })
+    .then(() => {
       console.log(`${file} saved`);
+    })
+    .catch((error) => {
+      throw error;
     });
-  });
 };
 
 let file = process.argv.slice(2).shift();
 alterFile(file);
+
+module.exports = exports = {readFile, modifyContents, writeFile};
